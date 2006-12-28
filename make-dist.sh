1ea5a8fea7cfc2fbc66c94bc5c967dd971fb5b55
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-28 20:52:45 +0000

    * src/Makefile.am   - Fix libsscm.la build on !USE_LIBSSCM. Thanks Etsushi
    * Makefile.am
    * make-dist.sh
      - Add alternative repository URLs
    
    * RELNOTE
      - Update

--- make-dist.sh
+++ make-dist.sh
@@ -3,8 +3,9 @@
 MAKE=make
 
 UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
-SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
+#SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.2"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.2"
 
 svn export $LIBGCROOTS_URL libgcroots

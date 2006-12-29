63e42313a4175200ca4e0449a2616e4287eab0ac
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-29 13:20:08 +0000

    * test-c/Makefile.am   - Include lacking libgcroots.mk
    * make-dist.sh
      - (LIBGCROOTS_URL): Update to 0.1.3
    * Makefile.am
      - (DIST_SUM_LIST): Use $(distdir)
    
    * RELNOTE
    * NEWS
      - Update

--- make-dist.sh
+++ make-dist.sh
@@ -6,7 +6,8 @@ UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
 SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.2"
-LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.2"
+#LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
+LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.3"
 
 svn export $LIBGCROOTS_URL libgcroots
 (cd libgcroots && ./autogen.sh) && ./autogen.sh

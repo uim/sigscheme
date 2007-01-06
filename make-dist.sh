d68d7672f99cfa75558abd768f7414b829535428
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-01-06 04:19:25 +0000

    * configure.in * src/sigscheme.h   - Increase version to 0.7.3 * make-dist.sh   - Update package versions * Makefile.am   - Use $(distdir) for tag name * RELNOTE * NEWS   - Update

--- make-dist.sh
+++ make-dist.sh
@@ -5,9 +5,9 @@ MAKE=make
 UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.2"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.3"
 #LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
-LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.3"
+LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.4"
 
 svn export $LIBGCROOTS_URL libgcroots
 (cd libgcroots && ./autogen.sh) && ./autogen.sh

e722befce45051e35270730000bb84165a89965c
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-03-14 16:28:01 +0000

    * make-dist.sh : Update for 0.7.5.

--- make-dist.sh
+++ make-dist.sh
@@ -5,7 +5,7 @@ MAKE=make
 UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.4"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.5"
 #LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.4"
 

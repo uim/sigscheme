ba92aa5d29cbbc7e611a4c26502a04319a88cbd5
yamakenz <yamakenz@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-07-18 17:19:38 +0000

    * make-dist.sh   - (SSCM_REPOSITORY): Update to 0.8.0   - (LIBGCROOTS_URL): Update to 0.2.0
    * doc/release.txt
      - Fix incorrect examples

--- make-dist.sh
+++ make-dist.sh
@@ -5,9 +5,9 @@ MAKE=make
 UIM_REPOSITORY="http://uim.googlecode.com/svn"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.4"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.8.0"
 #LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
-LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.4"
+LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.0"
 
 svn export $LIBGCROOTS_URL libgcroots
 (cd libgcroots && ./autogen.sh) \

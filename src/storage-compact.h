ee46532ee589bbac02f01d9d802e3b97308b0204
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-11-04 12:30:10 +0000

    * src/storage-compact.h * src/sigscheme.h * src/alloc.c   - Port r4963:5038 from sigscheme-trunk (bug #12981).

--- src/storage-compact.h
+++ src/storage-compact.h
@@ -639,11 +639,14 @@ SCM_MISC_DECLARE_TYPE(SYMBOL, L1(0),
     (SCM_ASSERT(SCM_ALIGNED_SYMBOL_NAME(n)),                                 \
      SCM_SET_Y(SCM_SYMBOL_PTR(o), (scm_uintobj_t)(n) | SCM_MTAG_SYMBOL))
 #define SCM_ISAL_SYMBOL_INIT(o, n, c)                                        \
-    (SCM_ASSERT(SCM_ALIGNED_SYMBOL_NAME(n)),                                 \
-     SCM_INIT((o),                                                           \
-              (c),                                                           \
-              (scm_uintobj_t)(n) | SCM_MTAG_SYMBOL,                          \
-              SCM_PTAG_MISC))
+    do {                                                                     \
+        char *_s = scm_align_str(n);                                         \
+        (SCM_ASSERT(SCM_ALIGNED_SYMBOL_NAME(_s)),                            \
+         SCM_INIT((o),                                                       \
+                  (c),                                                       \
+                  (scm_uintobj_t)(_s) | SCM_MTAG_SYMBOL,                     \
+                  SCM_PTAG_MISC));                                           \
+    } while (0)
 #define SCM_CELL_SYMBOLP(c)            SCM_MISC_CELL_TYPEP((c), SYMBOL)
 #define SCM_CELL_SYMBOL_FIN(c)                                               \
     do {                                                                     \

64496b8d5faf719fa175a1e369fcb96c002987d6
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2008-02-19 09:59:33 +0000

    * README * RELNOTE * NEWS * make-dist.sh   - Update.

--- make-dist.sh
+++ make-dist.sh
@@ -2,10 +2,10 @@
 
 MAKE=make
 
-UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
+UIM_REPOSITORY="http://uim.googlecode.com/svn"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
-#SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.5"
+#SSCM_REPOSITORY="${UIM_REPOSITORY}/branches/sigscheme-0.7"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.7.6"
 #LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.4"
 

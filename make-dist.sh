151460a3cab09ad0080f8951cf1dfab902ea0172
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2010-07-15 08:06:05 +0000

    * configure.in * make-dist.sh * src/sigscheme.h * test/test-sscm-ext.scm   - Update version number for 0.8.4 * NEWS * RELNOTE   - Update

--- make-dist.sh
+++ make-dist.sh
@@ -5,9 +5,9 @@ MAKE=make
 UIM_REPOSITORY="http://uim.googlecode.com/svn"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.8.3"
+SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.8.4"
 #LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
-LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.1"
+LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.2"
 
 svn export $LIBGCROOTS_URL libgcroots
 (cd libgcroots && ./autogen.sh) \

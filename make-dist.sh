2cea6fd61e58ffb74b5495187025f6a5635af36d
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2010-12-28 08:10:27 +0000

    * test/test-sscm-ext.scm * configure.in * make-dist.sh * src/sigscheme.h   - version bump

--- make-dist.sh
+++ make-dist.sh
@@ -4,10 +4,10 @@ MAKE=make
 
 UIM_REPOSITORY="http://uim.googlecode.com/svn"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
-#SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.8.5"
-#LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
-LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.3"
+SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
+#SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.9.0"
+LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
+#LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.3"
 
 svn export $LIBGCROOTS_URL libgcroots
 (cd libgcroots && ./autogen.sh) \
--- test/test-sscm-ext.scm
+++ test/test-sscm-ext.scm
@@ -43,7 +43,7 @@
 (define ud (undef))
 
 (tn "sscm-version")
-(assert-equal? (tn) "0.8.5"      (sscm-version))
+(assert-equal? (tn) "0.9.0"      (sscm-version))
 
 (tn "%%current-char-codec")
 (assert-equal? (tn) "UTF-8"      (%%current-char-codec))

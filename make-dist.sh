9ba5237935fd34e946ab8ad87aa4a3cdaf16dbc2
nogu.dev <nogu.dev@gmail.com>
2012-02-26 11:10:26 +0900

    * make-dist.sh   - Replace `svn export' with `git submodule'.

--- make-dist.sh
+++ make-dist.sh
@@ -2,15 +2,8 @@
 
 MAKE=make
 
-UIM_REPOSITORY="http://uim.googlecode.com/svn"
-TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
-SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
-#SSCM_REPOSITORY="${TAGS_REPOSITORY}/sigscheme-0.9.0"
-LIBGCROOTS_URL="${UIM_REPOSITORY}/libgcroots-trunk"
-#LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.3"
-
-svn export $LIBGCROOTS_URL libgcroots
-(cd libgcroots && ./autogen.sh) \
+git submodule update --init 
+(cd libgcroots && git checkout master && ./autogen.sh) \
 && ./autogen.sh \
 || { echo 'autogen failed.' && exit 1; }
 

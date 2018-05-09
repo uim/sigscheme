31b7e64efaf6c75be395a57adde37b939b797aed
Kouhei Sutou <kou@clear-code.com>
2018-05-09 13:28:07 +0900

    Use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-aclocal -I m4 \
-  && libtoolize --force --copy \
-  && autoheader \
-  && automake --add-missing --foreign --copy \
-  && autoconf
+${AUTORECONF:-autoreconf} --force --install "$@"
